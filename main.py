8809aa0b88b6b9ef654181764674984f00e000a6
Irina <Irina739791739791@gmail.com>
2023-03-24 22:08:50 +0300

    Создала функцию, которая выводит расширение файла

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+from typing import Union
+
+
+def get_expansion(name_file: str) -> Union [str, None]:
+    """Принимает имя файла и выводит его расширение
+
+    :param name_file (str): имя файла
+    :return:
+        str: расширение файла
+        None: Расширение у файла определить невозможно
+    """
+    try:
+        index = name_file.rfind(".")
+        assert index != -1, f"Расширение у файла определить невозможно"
+        return name_file[index + 1:]
+    except AssertionError as e:
+        print(e)
 
 
 def execute_application():
-
 
 
 
